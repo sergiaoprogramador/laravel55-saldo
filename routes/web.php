cd63cba148ee0e7835d0593de8e23e39c9938ad7
Sergio Ramos <sergio.b.snp@gmail.com>
2019-05-16 22:54:04 -0400

    aprendendo laravel 5.5

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin'], function() {
+    Route::get('admin', 'AdminController@index')->name('admin.home');
+});
+
+Route::get('/', 'Site\SiteController@index')->name('home');
+
+Auth::routes();
